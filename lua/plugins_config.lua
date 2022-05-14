b565a8d43c8a30399f10be81d182d4aee52b9441
Usiel Rivas <usielrivas00@gmail.com>
2022-05-14 08:34:30 -0600

    gitsing simbolos

--- lua/plugins_config.lua
+++ lua/plugins_config.lua
@@ -4,7 +4,15 @@ require "nvim-tree".setup {
         hide_root_folder = true
     }
 }
-require("gitsigns").setup()
+require("gitsigns").setup(
+signs = {
+    add          = {hl = 'GitSignsAdd'   , text = '│', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'},
+    change       = {hl = 'GitSignsChange', text = '│', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
+    delete       = {hl = 'GitSignsDelete', text = '_', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
+    topdelete    = {hl = 'GitSignsDelete', text = '‾', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
+    changedelete = {hl = 'GitSignsChange', text = '~', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
+  },
+)
 require("nvim-autopairs").setup {}
 require("nvim_comment").setup()
 
